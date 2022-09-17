6e6b93a9adea6c016a4e6d86baddc850cad556f4
asus <asus@Brainy-Dream>
2022-09-17 17:07:06 +0300

    Variables

---
+++ variables/src/variables/variables.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int number = 15;
+        String x = "kitap sayısı:";
+        System.out.println(x + number );
+        System.out.println(x + number );
+        System.out.println(x + number );
+
+       
+	}
+
+}
