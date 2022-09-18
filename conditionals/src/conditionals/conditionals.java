96c6f8be7e7aa780a63cb2371bfc4767bf381e50
asus <asus@Brainy-Dream>
2022-09-18 23:04:36 +0300

    conditionals (if blokları)

---
+++ conditionals/src/conditionals/conditionals.java
@@ -0,0 +1,52 @@
+package conditionals;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 
+		//EXAMPLE 1
+	  int number = 1;
+      if(number > 1) {
+    	  System.out.println("more than 1");
+      }else if (number < 1){
+          System.out.println("number is less than 1");
+      }else {
+    	  System.out.println("die nummer ist eins :)");
+      }  
+      
+      //EXAMPLE 2
+      String harf = "b";
+      if(harf == "a") {
+    	  System.out.println("a harfini seçtiniz :)");
+      }else{
+		  System.out.println("a harfinden başka harf seçtiniz ^.^");
+     
+      }
+      
+      //EXAMPLE 3 
+           
+      int y = 9;
+      
+      if (y == 729 ) {
+    	  System.out.println("y, 9'un 3. kuvvetidir.");
+      }else {
+    	  System.out.println("y, 9'un 3. kuvveti değildir.");
+    	  
+     //EXAMPLE 4
+    	
+      int x = 4 ;
+      if (x*4 == 16) {
+    	  System.out.println("x=4'tür");
+      }else if (x*x==81) {
+    	  System.out.println("x=9'dur");
+      }else {
+    	  System.out.println("x is different than 4 and 9");
+      }
+      }
+    	  
+      }
+      }
+    	 
+     
+	
