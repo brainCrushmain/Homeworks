175c952a9fd92a4cbf40a4a3458538fe3978376b
asus <asus@Brainy-Dream>
2022-09-20 22:17:36 +0300

    while loop is added

--- LoopDemo/src/ForLoops.java
+++ LoopDemo/src/ForLoops.java
@@ -3,13 +3,33 @@ public class ForLoops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        for(int i=1;i<10;i++) {
+       
+		//FOR LOOP
+		for(int i=1;i<10;i++) {
           System.out.println(i);
         }
         for(int a=20;a<1000;a+=2) {
         	System.out.println(a);
         }
         System.out.println("loop is over, babe");
-	}
 
-}
+         
+        
+        
+        //WHILE LOOP
+        
+        int i = 10;
+        while(i<20) {
+        	System.out.println(i);
+        	i+=4;}
+        
+        int a = 20;
+        while(a>30) {
+        	System.out.println(a);
+        	a+=5;}
+        System.out.println("while loop is over");
+        }}
+        
+        
+
+          
