2ff569381837ae72ab2950180ea1dce9fe045d19
asus <asus@Brainy-Dream>
2022-09-20 21:08:23 +0300

    sayı sıralama, belirli bir düzende

---
+++ LoopDemo/src/ForLoops.java
@@ -0,0 +1,15 @@
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for(int i=1;i<10;i++) {
+          System.out.println(i);
+        }
+        for(int a=20;a<1000;a+=2) {
+        	System.out.println(a);
+        }
+        System.out.println("loop is over, babe");
+	}
+
+}
